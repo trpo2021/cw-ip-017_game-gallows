855a25e9f8f40be6e5ea4826e7482ebf803be532
Artem Oshepkov <artemoshepkov@users.noreply.github.com>
2021-06-01 14:17:18 +0700

    Format functions.h

--- src/libgallows/functions.h
+++ src/libgallows/functions.h
@@ -1,10 +1,14 @@
 #pragma once
-#include <string>
 #include <SFML/Graphics.hpp>
+#include <string>
 int CheckLetter(std::string word, int WORDSIZE, int a);
-void FillingIndexArray(std::string word, int WORDSIZE, int NUMBERLETTERS, int* IndexWord);
-int SumMistakes(bool* Markers, std::string word, int NUMBERLETTERS, int WORDSIZE);
-int ManYouRight(bool* Markers, std::string word, int NUMBERLETTERS, int WORDSIZE);
+void FillingIndexArray(
+        std::string word, int WORDSIZE, int NUMBERLETTERS, int* IndexWord);
+int SumMistakes(
+        bool* Markers, std::string word, int NUMBERLETTERS, int WORDSIZE);
+int ManYouRight(
+        bool* Markers, std::string word, int NUMBERLETTERS, int WORDSIZE);
 void Theme_Word(std::string& tema, std::string& word);
 int non_repeating_characters(int amount, std::string word);
-void ResetGame(bool& button, sf::Sprite& ResetGameSprite, sf::RenderWindow& window);
+void ResetGame(
+        bool& button, sf::Sprite& ResetGameSprite, sf::RenderWindow& window);
